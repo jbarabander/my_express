a7345923590f5c4fb568073968529137d3c918dc
Justin Barabander <jbarabander@gmail.com>
2016-10-19 14:33:45 -0400

    More work on application

--- application.js
+++ application.js
@@ -3,7 +3,6 @@ var Router = require('./router');
 var sq = require('speed-query');
 function Application () {
   this._router = new Router();
-  this.server;
 }
 
 Application.prototype.get = function () {
@@ -18,16 +17,20 @@ Application.prototype.use = function () {
   this._router.use.apply(this._router, arguments);
 }
 
+Application.prototype.handle = function (req, res) {
+  var queryStr = req.url.split('?')[1];
+  req.query = queryStr ? sq.serialize(queryStr) : {};
+  this._router.handle.apply(this._router, arguments);
+}
+
 Application.prototype.listen = function (port, cb) {
   var portToUse = typeof port === 'number' ? port : 3000;
   var self = this;
-  this.server = http.createServer();
-  this.server.on('request', function (req, res) {
-    var queryStr = req.url.split('?')[1];
-    req.query = queryStr ? sq.serialize(queryStr) : {};
-    self._router.handle(req, res);
-  });
-  return this.server.listen(portToUse, cb);
+  var handler = function (req, res) {
+    return self.handle(req, res);
+  }
+  var server = http.createServer(handler);
+  return server.listen(portToUse, cb);
 }
 
 module.exports = Application;
