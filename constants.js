fd3842b1eb9f1ada93719929a2cc0b93ac677cd9
Justin Barabander <jbarabander@gmail.com>
2016-10-19 10:35:44 -0400

    working on use functionality

---
+++ constants.js
@@ -0,0 +1,8 @@
+module.exports = {
+  POST: 'POST',
+  USE: 'USE',
+  GET: 'GET',
+  PUT: 'PUT',
+  DELETE: 'DELETE',
+  ALL: 'ALL'
+}
--- router.js
+++ router.js
@@ -1,42 +1,26 @@
 var EventEmitter = require('events');
-
+var constants = require('./constants');
 function Router () {
   this.middleware = [];
 }
 
 Router.prototype.post = function (path, cb) {
-  var pathToRegister = typeof path === 'string' && path ? path : '/';
-  var hanlderToRegister;
-  if (typeof path === 'function') {
-    handlerToRegister = path
-  } else if (typeof cb === 'function') {
-    handlerToRegister = cb;
-  } else {
-    throw new TypeError('middleware handler must be a function');
-  }
+  var pathAndHandlerObj = pickPathAndHandler(path, cb);
   var entryToRegister = {
-    path: pathToRegister,
-    handler: handlerToRegister,
-    method: 'POST'
+    path: pathAndHandlerObj.path,
+    handler: pathAndHandlerObj.handler,
+    method: constants.POST
   }
   this.middleware.push(entryToRegister);
 }
 
 Router.prototype.get = function (path, cb) {
-  var pathToRegister = typeof path === 'string' && path ? path : '/';
-  var hanlderToRegister;
-  if (typeof path === 'function') {
-    handlerToRegister = path
-  } else if (typeof cb === 'function') {
-    handlerToRegister = cb;
-  } else {
-    throw new TypeError('middleware handler must be a function');
-  }
+  var pathAndHandlerObj = pickPathAndHandler(path, cb);
   var entryToRegister = {
-    path: pathToRegister,
-    handler: handlerToRegister,
-    method: 'POST'
-  }
+    path: pathAndHandlerObj.path,
+    handler: pathAndHandlerObj.handler,
+    method: constants.GET
+  };
   this.middleware.push(entryToRegister);
 }
 
@@ -44,10 +28,44 @@ Router.prototype.handle = function (req, res) {
   var pathToFind = req.url;
   var methodToFind = req.method;
   for (var i = 0; i < this.middleware.length; i++) {
-    var currentMiddleware = this.middleware[i];
-    if (pathToFind === currentMiddleware.path) {
-      return currentMiddleware.handler(req, res);
+    var currentMethod = this.middleware[i].method;
+    var currentPath = this.middleware[i].path;
+    var currentHandler = this.middleware[i].handler;
+    if (pathToFind === currentPath) {
+       if (methodToFind === currentMethod) {
+         return currentHandler(req, res);
+       } else if (currentMethod === constants.USE) {
+         if (currentHandler instanceof Router) {
+           currentHandler.handle(req, res);
+         }
+       }
     }
   }
 }
+
+Router.prototype.use = function (path, cb) {
+  var pathAndHandlerObj = pickPathAndHandler(path, cb, true);
+  var entryToRegister = {
+    path: pathAndHandlerObj.path,
+    handler: pathAndHandlerObj.handler,
+    method: constants.USE
+  }
+}
+
+function pickPathAndHandler (path, cb, allowRouter) {
+  var pathToRegister = typeof path === 'string' && path ? path : '/';
+  var handlerToRegister;
+  if (typeof path === 'function' || (allowRouter && path instanceof Router)) {
+    handlerToRegister = path
+  } else if (typeof cb === 'function' || (allowRouter && cb instanceof Router)) {
+    handlerToRegister = cb;
+  } else {
+    throw new TypeError('middleware handler must be a function');
+  }
+  return {
+    path: pathToRegister,
+    handler: handlerToRegister
+  }
+}
+
 module.exports = Router;
